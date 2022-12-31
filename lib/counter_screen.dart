b05bcb2b67aa569aecb1b580f14817517c35870c
shajith <shajith5020@gmail.com>
2022-12-31 16:16:52 +0530

    counter UI completed

---
+++ lib/counter_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  CounterScreen({Key? key}) : super(key: key);
+
+  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Demo Home Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:state_demo/counter_screen.dart';
+
+void main() => runApp(MaterialApp(
+      home: CounterScreen(),
+    ));
